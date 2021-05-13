50963d126f149cfdf4142c1415290b5c436d7998
Joseph <hejops1@gmail.com>
2021-05-13 19:26:19 +0200

    create .xinitrc

--- post.sh
+++ post.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
 set -eu
 
-systemctl start NetworkManager.service
-systemctl enable NetworkManager.service
+[ "$(whoami)" != joseph ] && exit
+
+# installs a bare minimum graphical environment after a successful install
+
+systemctl status NetworkManager.service | grep running || {
+	systemctl start NetworkManager.service
+	systemctl enable NetworkManager.service
+}
 
 ping -c 1 archlinux.org || nmtui
 
 # https://www.davidtsadler.com/posts/installing-st-dmenu-and-dwm-in-arch-linux/
-sudo pacman -S base-devel git libx11 libxft xorg-server xorg-xinit terminus-font
+sudo pacman -S base-devel libx11 libxft xorg-server xorg-xinit terminus-font libxinerama rxvt-unicode ranger
 
 cd
 git clone https://github.com/hejops/dwm
 cd dwm
+make clean
 sudo make install
+
+echo dwm > .xinitrc
+
+echo "Graphical environment configured. To start dwm, run startx"
