1c01d9631f3d9be0f72c11eeeb43b44da5617458
Joseph <hejops1@gmail.com>
2023-12-08 08:50:01 +0000

    restructure

--- mac.sh
+++ mac.sh
@@ -6,80 +6,114 @@ set -euo pipefail
 # cd arch
 # bash mac.sh
 
-# 1. get github token -- file transfer, or type manually
-# 2. git config --global credential.helper store
-# (else get password prompt, which is insecure/outdated)
-# 3. git clone https://github.com/hejops/dotfiles
+# homebrew is avoided at all costs
 
-# must redo after every system update...
-xcode-select --install || :
+build() {
 
-IFS=" " read -r -a MAIN <<< "$(perl < ./packages_mac.txt -nle'print if m{^[^# 	]+}' | xargs)"
-brew install "${MAIN[@]}"
+	:
 
-exit
+	# build: macports nvim conda xcode
 
-if ! [[ -f "$HOME/.git-credentials" ]]; then
-	git config --global credential.helper store
-	echo "Setting up Github PAT..."
-	open "https://github.com/settings/tokens/new" > /dev/null
-	read -r -p "PAT: " PAT < /dev/tty
-	echo "https://hejops:$PAT@github.com" |
-		tr -d ' ' |
-		tee "$HOME/.git-credentials"
+	# https://www.macports.org/install.php
+	# https://github.com/macports/macports-base/releases/download/v2.8.0/MacPorts-2.8.0-13-Ventura.pkg
 
-	# get dotfiles and scripts
+	# command -v port
+	# /opt/local/bin/port
 
-	cd
-	git clone https://github.com/hejops/dotfiles
+	# must redo after every system update...
+	xcode-select --install || :
 
-fi
+	# 'sudo xcodebuild -license' not required, apparently
 
-rm -rf "$HOME/.mozilla"
-
-# delete most dirs in config (or rather, rsync select dirs out)
-
-# rsync -vua ~/dotfiles/ .
-# rm -r ~/dotfiles
-
-# xrdb -merge ~/.Xresources
-
-# TODO: separate window?
-vim
-
-# cd
-# git clone https://github.com/hejops/scripts
-# bash ~/scripts/links ~/scripts
-
-remove_dock_icons() {
-	# https://stackoverflow.com/a/20695955
-
-	# TODO: determine end condition
-
-	sudo -u $USER /usr/libexec/PlistBuddy -c "Delete persistent-apps:0" ~/Library/Preferences/com.apple.dock.plist
-
-	# dloc=$(defaults read com.apple.dock persistent-apps | grep file-label | awk '/Notes/  {printf NR}')
-	# dloc=$((dloc - 1))
-	# echo $dloc
-	# sudo -u $USER /usr/libexec/PlistBuddy -c "Delete persistent-apps:$dloc" ~/Library/Preferences/com.apple.dock.plist
-
-	# # must delete item from com.apple.dock.plist agian,or won't change
-	# dloc=$(defaults read com.apple.dock persistent-apps | grep file-label | awk '/Photo Booth/  {printf NR}')
-	# dloc=$((dloc - 1))
-	# echo $dloc
-	# sudo -u $USER /usr/libexec/PlistBuddy -c "Delete persistent-apps:$dloc" ~/Library/Preferences/com.apple.dock.plist
-
-	sleep 3
-	# Restart Dock to persist changes
-	osascript -e 'delay 3' -e 'tell Application "Dock"' -e 'quit' -e 'end tell'
 }
 
-# TODO:
+# port: ranger lowdown shfmt npm? pcregrep pip?
 
-# keyboard shortcuts: terminal / ranger / firefox
-# autostart
+# pip: black
+# npm i -g: prettier bash-language-server
 
-# # disable startup sound
-# sudo nvram StartupMute=%01
+# IFS=" " read -r -a MAIN <<< "$(perl < ./packages_mac.txt -nle'print if m{^[^# 	]+}' | xargs)"
 
-# reverse scroll direction
+dotfiles() {
+
+	# .bashrc
+	# .bash_aliases
+	# .vim
+
+	dirs_to_copy=(
+
+		kitty
+		nvim
+		ranger
+		readline
+		tridactyl
+	)
+
+	for x in "${dirs_to_copy[@]}"; do
+		ln -sf ~/dotfiles/.config/"$x" ~/.config
+	done
+
+	exit
+
+	# git config --global user.email hejops1@gmail.com
+	# git config --global user.name Joseph
+
+	# TODO: remove and stop tracking sensitive files:
+	# .cron
+	# ~/.config/nicotine
+	# ~/.config/newsboat
+	# ~/.config/tridactyl/tridactylrc
+
+	if ! [[ -f "$HOME/.git-credentials" ]]; then
+		git config --global credential.helper store
+		echo "Setting up Github PAT..."
+		open "https://github.com/settings/tokens/new" > /dev/null
+		read -r -p "PAT: " PAT < /dev/tty
+		echo "https://hejops:$PAT@github.com" |
+			tr -d ' ' |
+			tee "$HOME/.git-credentials"
+
+		# get dotfiles and scripts
+
+		cd
+		git clone https://github.com/hejops/dotfiles
+
+	fi
+
+	rm -rf "$HOME/.mozilla"
+
+	# delete most dirs in config (or rather, rsync select dirs out)
+
+	# rsync -vua ~/dotfiles/ .
+	# rm -r ~/dotfiles
+
+	# xrdb -merge ~/.Xresources
+
+	# fonts dir: /Library/Fonts
+
+	# 1. get github token -- file transfer, or type manually
+	# 2. git config --global credential.helper store
+	# (else get password prompt, which is insecure/outdated)
+	# 3. git clone https://github.com/hejops/dotfiles
+
+}
+
+dock_settings() {
+	# removes nearly everything; the rest can be manually removed
+	# defaults write com.apple.dock persistent-apps '()'
+
+	# https://macos-defaults.com
+	defaults write com.apple.dock "orientation" -string "left"
+	defaults write com.apple.dock "show-recents" -bool "false"
+	defaults write com.apple.dock "static-only" -bool "true"
+	defaults write com.apple.dock "tilesize" -int "30"
+
+	# keyboard shortcuts: terminal / ranger / firefox
+	# autostart
+
+	# # disable startup sound
+	# sudo nvram StartupMute=%01
+
+	# reverse scroll direction
+
+}
